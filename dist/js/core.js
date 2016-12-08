bca103105a3e2d8c65a10a493fde31da45e786d7
Michael Juliano <michael.juliano@markit.com>
2016-12-08 12:49:17 -0700

    IE fix for creating elements

--- dist/js/core.js
+++ dist/js/core.js
@@ -410,11 +410,17 @@ $.wrap.dt        = $.wrap.dd;
 
 $.markup = function (s) {
 
+    var d = document;
+
+    if (!s) {
+        return d.createDocumentFragment();
+    }
+
     // html5 templates
     // most browsers support this method and
     // is much faster than the latter.
-    var d = document,
-        c = d.createElement('template'), f;
+    var c = d.createElement('template'),
+        f, p;
 
     if (c.content) {
         c.innerHTML = s;
@@ -428,13 +434,18 @@ $.markup = function (s) {
         i = 0;
 
     c = d.createElement('div');
-    f = d.createDocumentFragment();
-
     c.innerHTML = a[1] + s + a[2];
 
+    p = c.firstChild;
+    f = d.createDocumentFragment();
+
     while (i < a[0]) {
-        f.appendChild(c.firstChild);
+        p = p.firstChild;
         i++;
+    }
+
+    if (p) {
+        f.appendChild(p);
     }
 
     return f;
--- src/js/core/dom.js
+++ src/js/core/dom.js
@@ -130,11 +130,17 @@ $.wrap.dt        = $.wrap.dd;
 
 $.markup = function (s) {
 
+    var d = document;
+
+    if (!s) {
+        return d.createDocumentFragment();
+    }
+
     // html5 templates
     // most browsers support this method and
     // is much faster than the latter.
-    var d = document,
-        c = d.createElement('template'), f;
+    var c = d.createElement('template'),
+        f, p;
 
     if (c.content) {
         c.innerHTML = s;
@@ -148,13 +154,18 @@ $.markup = function (s) {
         i = 0;
 
     c = d.createElement('div');
-    f = d.createDocumentFragment();
-
     c.innerHTML = a[1] + s + a[2];
 
+    p = c.firstChild;
+    f = d.createDocumentFragment();
+
     while (i < a[0]) {
-        f.appendChild(c.firstChild);
+        p = p.firstChild;
         i++;
+    }
+
+    if (p) {
+        f.appendChild(p);
     }
 
     return f;
