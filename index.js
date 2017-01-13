d409f7d9c73c5a0fd361357a427b94007afea393
Eric Nolte <eric.nolte@markit.com>
2017-01-13 15:25:24 -0700

    Update namespaces and index.js to work with module loaders

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
+var mk = require('./dist/js/core');
 
-var express = require('express'),
-	middleware = require('./middleware/middleware'),
-	routes = require('./routes/routes'),
-	app = express();
-
-middleware(express, app, __dirname);
-
-routes(express, app);
-
-app.listen(5280, function () {
-  console.log('Mk Documentation App Loaded on port [localhost:]5280');
-});
+module.exports = mk;
